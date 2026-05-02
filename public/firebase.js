6c1a9d5e2aa242d8c4d7e5a0b1becdfebada8d9f
huzaifa <idkmyname9009@gmail.com>
2026-05-02 12:25:46 +0530

    firebase.js

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBwSKgUGCzi5Xg8ex_JnTqcryRo6gmnU5k",
+    authDomain: "hackathonproject-ac923.firebaseapp.com",
+    projectId: "hackathonproject-ac923",
+    storageBucket: "hackathonproject-ac923.firebasestorage.app",
+    messagingSenderId: "371298066064",
+    appId: "1:371298066064:web:6aec13d2a9232326fcc215"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
